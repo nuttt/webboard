c3525ad39f71436961c9ea036a641ba055d3f89e
Vee <happierv@gmail.com>
2014-02-16 17:07:21 +0700

    codeigniter

---
+++ application/models/person.php
@@ -0,0 +1,39 @@
+<?php
+class Person extends CI_Model {
+
+	var $title   = '';
+	var $content = '';
+	var $date    = '';
+
+	function __construct()
+	{
+		// Call the Model constructor
+		parent::__construct();
+	}
+	
+	function test()
+	{
+		//$query = $this->db->get('person', 10);
+		$query = $this->db->query("SELECT * FROM person");
+		return $query->result();
+	}
+
+	// function insert_entry()
+	// {
+	// 	$this->title   = $_POST['title']; // please read the below note
+	// 	$this->content = $_POST['content'];
+	// 	$this->date    = time();
+
+	// 	$this->db->insert('entries', $this);
+	// }
+
+	// function update_entry()
+	// {
+	// 	$this->title   = $_POST['title'];
+	// 	$this->content = $_POST['content'];
+	// 	$this->date    = time();
+
+	// 	$this->db->update('entries', $this, array('id' => $_POST['id']));
+	// }
+
+}
