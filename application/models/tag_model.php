3ad433140677307689b535e8e08100625a107728
Nuttapon Pattanavijit <nuttt.p@gmail.com>
2014-02-21 18:01:16 +0700

    update authentication session

--- application/models/tag_model.php
+++ application/models/tag_model.php
@@ -6,4 +6,14 @@ class tag_model extends CI_Model {
 		$query = $this->db->query("SELECT TAG.TAG_ID, TAG.NAME FROM TAG INNER JOIN (SELECT TAG_ID FROM TOPIC_TAG WHERE TOPIC_ID=".$post_id.") TT ON TAG.TAG_ID = TT.TAG_ID");
 		return $query->result();
 	}
+
+	function get_tags(){
+		$query = $this->db->query("SELECT * FROM TAG");
+		return $query->result();
+	}
+
+	function get_tags_with_topic_num(){
+		$query = $this->db->query("SELECT TAG.TAG_ID, TAG.NAME, count(*) as NUM FROM TAG INNER JOIN TOPIC_TAG ON TAG.TAG_ID = TOPIC_TAG.TAG_ID GROUP BY TAG.TAG_ID, TAG.NAME");
+		return $query->result();
+	}
 }
