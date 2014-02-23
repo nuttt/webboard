b0a77f0c80919f2ebf0e7a94bd7ac0253935da2a
Nuttapon Pattanavijit <nuttt.p@gmail.com>
2014-02-23 16:59:07 +0700

    add dashboard

--- application/models/ban_model.php
+++ application/models/ban_model.php
@@ -18,49 +18,6 @@ class Ban_model extends CI_Model {
 		return $query->result();
 	}
 
-	public function verify_person($email, $password){
-		$query = $this->db->query("SELECT $this->attributes FROM person WHERE email = '".$email."' AND password = '".sha1($password)."' AND ROWNUM <= 1");
-		if($query->num_rows() > 0){
-			return $query->row();
-		}
-		return false;
-	}
-
-	public function get_person($id){
-		$query = $this->db->query("SELECT $this->attributes FROM person WHERE person_id = '".$id."' AND ROWNUM <= 1");
-		if($query->num_rows() > 0){
-			return $query->row();
-		}
-		return false;
-	}
-
-	function is_admin($person_id){
-		$query = $this->db->query("SELECT person_id FROM person_admin");
-		return $query->num_rows() > 0;
-	}
-
-	function get_person_profile($id){
-		$query = $this->db->query("");
-	}
-
-	function get_members(){
-		$query = $this->db->query("SELECT $this->attributes FROM person WHERE person_id not in (SELECT person_id FROM person_moderator) and person_id not in (SELECT person_id FROM person_admin)");
-		return $query->result();
-	}
-
-	function get_moderators(){
-		$query = $this->db->query("SELECT $this->attributes FROM person WHERE person_id in (SELECT person_id FROM person_moderator)");
-		return $query->result();
-	}
-
-	function get_admins(){
-		$query = $this->db->query("SELECT $this->attributes FROM person WHERE person_id in (SELECT person_id FROM person_admin)");
-		return $query->result();
-	}
-
-	function get_person_number(){
-		$query = $this->db->query("SELECT count(*) as num FROM person");
-		return $query->row()->NUM;
-	}
+	
 
 }
