1e3d50093a6266fa722dcdc8ef139bc36deb045b
Nuttapon Pattanavijit <nuttt.p@gmail.com>
2014-02-25 13:00:37 +0700

    promote user

--- application/views/post/hook-vote-topic.php
+++ application/views/post/hook-vote-topic.php
@@ -1,12 +1,12 @@
 
 							<?php if($person_loggedin): // user logged in ?>
-								<a href="" class="btn btn-danger btn-xs vote vote-down">
+								<a href="<?php echo base_url('post/vote_down/'.$post->POST_ID.'?return=post/view/'.$post->POST_ID) ?>" class="btn btn-danger btn-xs vote vote-down">
 									<span class="glyphicon glyphicon-thumbs-down"></span>
 								</a><!--vote-->
 								<span class="current-score">
 									<?php echo $post->VOTE; ?>
 								</span>
-								<a href="" class="btn btn-success btn-xs vote vote-up">
+								<a href="<?php echo base_url('post/vote_up/'.$post->POST_ID.'?return=post/view/'.$post->POST_ID) ?>" class="btn btn-success btn-xs vote vote-up">
 									<span class="glyphicon glyphicon-thumbs-up"></span>
 								</a><!--vote-->
 							<?php else: ?>
