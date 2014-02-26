e615d85542be6767b7cf3a5d72312f7195c2cb03
Nuttapon Pattanavijit <nuttt.p@gmail.com>
2014-02-27 02:54:37 +0700

    add flash fir post model

--- application/models/post_model.php
+++ application/models/post_model.php
@@ -30,7 +30,9 @@ class Post_model extends CI_Model {
 		}
 		$this->load->model('tag_model');
 		$this->load->model('post_reply_model');
-		$post_query = $this->db->query("SELECT PERSON.PERSON_ID, POST.POST_ID, to_char(POST.TIME,'DY DD-Mon-YYYY HH24:MI')AS TIME, POST_TOPIC.TITLE, PERSON.DISPLAY_NAME, POST_TOPIC.VISIT FROM POST INNER JOIN POST_TOPIC ON POST.POST_ID = POST_TOPIC.POST_ID INNER JOIN PERSON ON PERSON.PERSON_ID = POST.PERSON_ID ".$filter." ORDER BY ".$sort);
+		$q = "SELECT PERSON.PERSON_ID, POST.POST_ID, to_char(POST.TIME,'DY DD-Mon-YYYY HH24:MI')AS TIME, POST_TOPIC.TITLE, PERSON.DISPLAY_NAME, POST_TOPIC.VISIT FROM POST INNER JOIN POST_TOPIC ON POST.POST_ID = POST_TOPIC.POST_ID INNER JOIN PERSON ON PERSON.PERSON_ID = POST.PERSON_ID ".$filter." ORDER BY ".$sort;
+		$post_query = $this->db->query($q);
+		$this->sesssion->set_flashdata('query', $q);
 		$posts = $post_query->result();
 		foreach($posts as $post){
 			$post->TAGS = $this->tag_model->get_tag($post->POST_ID);
@@ -65,20 +67,22 @@ class Post_model extends CI_Model {
 			$filter = "INNER JOIN TOPIC_TAG ON POST.POST_ID = TOPIC_TAG.TOPIC_ID INNER JOIN TAG ON TAG.TAG_ID = TOPIC_TAG.TAG_ID AND TAG.NAME = '".$tag_filter."' ";
 			
 		}
-		$topic_query = $this->db->query("SELECT PERSON.PERSON_ID, POST.POST_ID, 
-										to_char(POST.TIME,'DY DD-Mon-YYYY HH24:MI')AS TIME, 
-										POST_TOPIC.TITLE, PERSON.DISPLAY_NAME, POST_TOPIC.VISIT,
-										CASE WHEN sum(VOTES.STATUS) IS NULL THEN 0 ELSE sum(VOTES.STATUS) END 
-										AS VOTE
-										FROM POST 
-										INNER JOIN PERSON ON PERSON.PERSON_ID = POST.PERSON_ID AND PERSON.PERSON_ID = "
-										.$person_id.$filter.
-										" INNER JOIN POST_TOPIC ON POST.POST_ID = POST_TOPIC.POST_ID 
-										LEFT  JOIN VOTES ON POST.POST_ID = VOTES.POST_ID
-										GROUP BY PERSON.PERSON_ID, POST.POST_ID, 
-										to_char(POST.TIME,'DY DD-Mon-YYYY HH24:MI'), 
-										POST_TOPIC.TITLE, PERSON.DISPLAY_NAME, POST_TOPIC.VISIT
-										ORDER BY ".$sort);
+		$q = "SELECT PERSON.PERSON_ID, POST.POST_ID, 
+					to_char(POST.TIME,'DY DD-Mon-YYYY HH24:MI')AS TIME, 
+					POST_TOPIC.TITLE, PERSON.DISPLAY_NAME, POST_TOPIC.VISIT,
+					CASE WHEN sum(VOTES.STATUS) IS NULL THEN 0 ELSE sum(VOTES.STATUS) END 
+					AS VOTE
+					FROM POST 
+					INNER JOIN PERSON ON PERSON.PERSON_ID = POST.PERSON_ID AND PERSON.PERSON_ID = "
+					.$person_id.$filter.
+					" INNER JOIN POST_TOPIC ON POST.POST_ID = POST_TOPIC.POST_ID 
+					LEFT  JOIN VOTES ON POST.POST_ID = VOTES.POST_ID
+					GROUP BY PERSON.PERSON_ID, POST.POST_ID, 
+					to_char(POST.TIME,'DY DD-Mon-YYYY HH24:MI'), 
+					POST_TOPIC.TITLE, PERSON.DISPLAY_NAME, POST_TOPIC.VISIT
+					ORDER BY ".$sort;
+		$topic_query = $this->db->query($q);
+		$this->sesssion->set_flashdata('query', $q);
 		$topics = $topic_query->result();
 		foreach($topics as $topic){
 			$topic->TAGS = $this->tag_model->get_tag($topic->POST_ID);
@@ -93,11 +97,13 @@ class Post_model extends CI_Model {
 	function get_topics_with_tag($tag_ID){
 		$this->load->model('tag_model');
 		$this->load->model('post_reply_model');
-		$post_query = $this->db->query("SELECT PERSON.PERSON_ID, POST.POST_ID, to_char(POST.TIME,'DY DD-Mon-YYYY HH24:MI')AS TIME, POST_TOPIC.TITLE, PERSON.DISPLAY_NAME " .
+		$q = "SELECT PERSON.PERSON_ID, POST.POST_ID, to_char(POST.TIME,'DY DD-Mon-YYYY HH24:MI')AS TIME, POST_TOPIC.TITLE, PERSON.DISPLAY_NAME " .
 			"FROM POST " .
 			"INNER JOIN POST_TOPIC ON POST.POST_ID = POST_TOPIC.POST_ID " .
 			"INNER JOIN PERSON ON PERSON.PERSON_ID = POST.PERSON_ID " .
-			"INNER JOIN TOPIC_TAG ON POST.POST_ID = TOPIC_TAG.TOPIC_ID AND TOPIC_TAG.TAG_ID = " . $tag_ID);
+			"INNER JOIN TOPIC_TAG ON POST.POST_ID = TOPIC_TAG.TOPIC_ID AND TOPIC_TAG.TAG_ID = " . $tag_ID;
+		$post_query = $this->db->query($q);
+		$this->sesssion->set_flashdata('query', $q);
 
 		$posts = $post_query->result();
 		foreach($posts as $post){
@@ -139,19 +145,24 @@ class Post_model extends CI_Model {
 	}
 
 	function create($post_data) {
+		$q = "";
 		$q_post = "INSERT INTO POST (CONTENT, TIME, STATUS, PERSON_ID) values ('".$post_data['content']."', systimestamp, ".$post_data['status'].", ".$post_data['person_id'].")";
+		$q .= $q_post;
 		$this->db->query($q_post);
 		$query = $this->db->query("SELECT POST_ID FROM POST WHERE ROWNUM = 1 ORDER BY POST_ID DESC");
 		$post_id = $query->first_row()->POST_ID;
 
 		$q_post_topic = "INSERT INTO POST_TOPIC (POST_ID, TITLE, VISIT) values ($post_id, '".$post_data['title']."', 0)";
 		$this->db->query($q_post_topic);
+		$q .= "<br>".$q_post_topic;
 
 		foreach($post_data['tag'] as $tag) {
-			echo $q_topic_tag =  "INSERT INTO TOPIC_TAG (TOPIC_ID, TAG_ID) values ($post_id, ".$tag.")";
+			$q_topic_tag =  "INSERT INTO TOPIC_TAG (TOPIC_ID, TAG_ID) values ($post_id, ".$tag.")";
 			$this->db->query($q_topic_tag);
+			$q .= "<br>".$q_topic_tag;
 		}
 
+		$this->sesssion->set_flashdata('query2', $q);
 		return $post_id;
 	}
 
@@ -198,11 +209,17 @@ class Post_model extends CI_Model {
 		$rm_query = "DELETE FROM POST WHERE POST_ID=".$post_id;
 		$this->db->query($rm_query);
 
+		$q = $rm_query;
+
 		$cleanup_query = "delete from post
 		where post_id not in (select post_id from post_topic)
 		and post_id not in (select post_id from post_reply)";
 		$this->db->query($rm_query);
 		$this->session->set_flashdata('message', 'Your post was deleted.');
+
+		$q .= "<br>".$cleanup_query;
+
+		$this->sesssion->set_flashdata('query2', $q);
 
 	}
 
